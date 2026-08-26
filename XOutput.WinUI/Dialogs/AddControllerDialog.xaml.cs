1caafaa7ff85444e7cad57157c16ddf5eaaed651
JCVERSA <251261529+JCVERSA@users.noreply.github.com>
2026-08-26 01:32:00 +0000

    Fix WinUI CI build: add using and move XBoxModel to Core
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- XOutput.WinUI/Dialogs/AddControllerDialog.xaml.cs
+++ XOutput.WinUI/Dialogs/AddControllerDialog.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Windows.UI;
+using XOutput.Devices;
 using XOutput.Devices.Input;
 using XOutput.Devices.XInput;
 

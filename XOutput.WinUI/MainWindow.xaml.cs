3729fcf163e4217e0395398395814dc70947bcd1
JCVERSA <251261529+JCVERSA@users.noreply.github.com>
2026-08-26 01:47:59 +0000

    Fix remaining WinUI/WPF CI build breakages
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- XOutput.WinUI/MainWindow.xaml.cs
+++ XOutput.WinUI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using WinRT;
 using H.NotifyIcon;
+using Microsoft.UI;
 using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
